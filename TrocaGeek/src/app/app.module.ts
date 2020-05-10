d994152e14c62fc4e5039a145aa55a8a1fc48f52
Lirian Bordignon <lirianbordignon26@gmail.com>
2020-05-10 16:26:46 -0300

    Criação dos Componentes, home, produtos, navbar, footer, login e cadastrar e sobre nos

--- TrocaGeek/src/app/app.module.ts
+++ TrocaGeek/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProdutosComponent,
+    LoginComponent,
+    CadastrarComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
