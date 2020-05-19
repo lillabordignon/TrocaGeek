76602054bef425b327b9df3cd0f072466a05037e
Lirian Bordignon <lirianbordignon@gmail.com>
2020-05-19 12:13:15 -0300

    Criação da página de Login

--- TrocaGeek/src/app/app-routing.module.ts
+++ TrocaGeek/src/app/app-routing.module.ts
@@ -7,11 +7,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { ContatoComponent } from './contato/contato.component';
 import { TermosDeUsoComponent } from './termos-de-uso/termos-de-uso.component';
 import { FAQComponent } from './faq/faq.component';
-<<<<<<< HEAD
 import { AdmComponent } from './adm/adm.component';
-=======
 import { UsuariosComponent } from './usuarios/usuarios.component';
->>>>>>> aa80bee5d9f8776dd7731cae23423bbc2f1151ed
 
 
 const routes: Routes = [
@@ -24,11 +21,8 @@ const routes: Routes = [
   { path: 'contato', component: ContatoComponent },
   { path: 'termos-de-uso', component: TermosDeUsoComponent },
   { path: 'faq', component: FAQComponent },
-<<<<<<< HEAD
-  { path: 'listausuarios', component: AdmComponent }
-=======
+  { path: 'listausuarios', component: AdmComponent },
   { path: 'usuarios', component: UsuariosComponent }
->>>>>>> aa80bee5d9f8776dd7731cae23423bbc2f1151ed
 
 ];
 
