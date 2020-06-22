efb1db40487858a5d2027b49c85883b504dc9e53
Leonardo <leomontagnolli@gmail.com>
2020-06-22 15:39:14 -0300

    Atualizando end point de atualizar usuario com senha crypto

--- TrocaGeek/src/main/java/com/generation/trocageek/controller/UsuarioController.java
+++ TrocaGeek/src/main/java/com/generation/trocageek/controller/UsuarioController.java
@@ -3,9 +3,12 @@ package com.generation.trocageek.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.trocageek.model.Usuario;
+import com.generation.trocageek.model.UsuarioEditar;
 import com.generation.trocageek.model.UsuarioLogin;
 import com.generation.trocageek.repository.UsuarioRepository;
 import com.generation.trocageek.service.UsuarioService;
@@ -31,6 +35,7 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+
 	
 	@GetMapping
 	public List<Usuario> listar () {
@@ -58,9 +63,25 @@ public class UsuarioController {
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 	
-	@PutMapping
-	public ResponseEntity<Usuario> atualizar (@RequestBody Usuario usuario) {
-		return ResponseEntity.ok(repository.save(usuario));
+	@PutMapping("/{id}")//futuramente extrair esse metodo para o service
+	@Transactional
+	public ResponseEntity<Object> atualizar (@PathVariable Long id,@RequestBody UsuarioEditar usuario) {
+		Optional<Usuario> optional = repository.findById(id);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		if(optional.isPresent()) {
+			if(encoder.matches(usuario.getSenhaAntiga(), optional.get().getSenha())) {
+				String senhaCrypt = encoder.encode(usuario.getSenha());
+				optional.get().setNome(usuario.getNome());
+				optional.get().setEmail(usuario.getEmail());
+				optional.get().setTelefone(usuario.getTelefone());
+				optional.get().setSenha(senhaCrypt);
+				
+				return ResponseEntity.ok(repository.save(optional.get()));
+			}
+			return ResponseEntity.badRequest().body("Senha atual não está correta !");
+		}
+		
+		return ResponseEntity.notFound().build();
 		
 	}
 	
---
+++ TrocaGeek/src/main/java/com/generation/trocageek/model/UsuarioEditar.java
@@ -0,0 +1,43 @@
+package com.generation.trocageek.model;
+
+public class UsuarioEditar {
+	private String nome;
+	private String senha;
+	private String senhaAntiga;
+	private String email;
+	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getSenhaAntiga() {
+		return senhaAntiga;
+	}
+	public void setSenhaAntiga(String senhaAntiga) {
+		this.senhaAntiga = senhaAntiga;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+
+}
