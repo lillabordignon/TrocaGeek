3808208323c79142be7298eaadc045728546fb7f
DeividsonGonini <deividsongonini@gmail.com>
2020-06-23 17:19:39 -0300

    Adicionado Minhas Negociações, com listando Vendas e Compras

--- TrocaGeek/src/main/java/com/generation/trocageek/repository/NegociacaoRepository.java
+++ TrocaGeek/src/main/java/com/generation/trocageek/repository/NegociacaoRepository.java
@@ -1,9 +1,14 @@
 package com.generation.trocageek.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.trocageek.model.Negociacao;
 
 public interface NegociacaoRepository extends JpaRepository<Negociacao, Long>{
 
+	public List<Negociacao> findByidVendedor_id(Long id);
+	
+	public List<Negociacao> findByidComprador_id(Long id);
 }
