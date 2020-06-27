dfb50a5dffdbf1cdbf57fe0066983bdef5155c3b
buenocas <cas.profissional@gmail.com>
2020-06-27 13:06:25 -0300

    Att rotas Minha Área

--- TrocaGeek-backEnd/TrocaGeek/src/main/java/com/generation/trocageek/model/UsuarioEditar.java
+++ TrocaGeek-backEnd/TrocaGeek/src/main/java/com/generation/trocageek/model/UsuarioEditar.java
@@ -2,7 +2,6 @@ package com.generation.trocageek.model;
 
 public class UsuarioEditar {
 	private String nome;
-	private String senha;
 	private String senhaAntiga;
 	private String email;
 	private String telefone;
@@ -12,12 +11,6 @@ public class UsuarioEditar {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
 	}
 	public String getSenhaAntiga() {
 		return senhaAntiga;
